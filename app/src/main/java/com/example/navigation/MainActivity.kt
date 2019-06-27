9c79c842108753aac6ed8f8686c6cd181cf41b33
Jian_Cheng <s5016335@gmail.com>
2019-06-27 17:36:24 +0800

    Add Coroutine

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -2,12 +2,43 @@ package com.example.navigation
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),CoroutineScope {
+    lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main+job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        job=Job()
+        launch {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+
+    fun loadData()=launch {
+        val iodata = async(Dispatchers.IO) {
+        }
+
+        val  data =iodata.await()
+
+    }
+
+    suspend fun  loaduser(){
+
+        withContext(Dispatchers.IO){
+
+        }
+
     }
 
 
